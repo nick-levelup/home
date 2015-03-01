4458b590622f98e29d8fc62db572ccf4e8edffb4
AObolonkova <aleksandraObolonkova@yandex.ru>
2015-03-01 16:51:48 +0200

    #12 DZ Aleksandra

---
+++ aleksandra/02032015dz#12/Task1/number.js
@@ -0,0 +1,16 @@
+var num;
+
+num = prompt("Введите число",'');
+num = +num;
+
+
+if (num === 1) {
+	alert("Число равно 1");
+} else if (num < 1) {
+	alert("Число меньше 1");
+} else if (num > 1) {
+	alert("Число больше 1");
+}
+
+
+
