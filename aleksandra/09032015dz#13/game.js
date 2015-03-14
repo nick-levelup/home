92d678d61be73fd138880cfb8f11b5b0225b0231
AObolonkova <aleksandraObolonkova@yandex.ru>
2015-03-14 20:01:35 +0200

    #13 DZ Aleksandra fix

--- aleksandra/09032015dz#13/game.js
+++ aleksandra/09032015dz#13/game.js
@@ -11,40 +11,39 @@ while (true) {
 function getStart() {
 
 scorePoints = 50;
-step = 0;
-points = 0;
-pointsNum = 0;
-pointsEsc = 0;
- 
-	while (points <= 50) {
+step = 0; points = 0; pointsNum = 0; pointsEsc = 0;
+minNum = 1; maxNum = 20;
 
-	//Функция вызова рендома
-	numRandom = getRandomInt(1,20);
+ 
+	while (points <= scorePoints) {
+
+		//Функция вызова рендома
+		numRandom = getRandomInt(minNum,maxNum);
 
     
-	num = prompt("Угадай число от 1 до 20",'');
-	num = +num;
-	step = step + 1;
+		num = prompt("Угадай число от " + minNum +  " до " + maxNum ,'');
+		num = +num;
+		step = step + 1;
 
-	if (num === null || num === '') {    //Отмена игроком
-   		break;
-	}  
+		if (num === null || num === '') {    //Отмена игроком
+   			break;
+		}  
 
-	//Функция подсчета баллов 
-	points = countPoints(num,numRandom,points);
+		//Функция подсчета баллов 
+		points = countPoints(num,numRandom,points);
 
-	pointsNum = pointsNum + points;
-	pointsEsc = (pointsNum <= 50 ) ? (50 - pointsNum) : '0';
+		pointsNum = pointsNum + points;
+		pointsEsc = (pointsNum <= scorePoints ) ? (scorePoints - pointsNum) : '0';
 
-	//Вывод информации в консоль для пользователя, о то сколько он заработал балов за ход и сколько ему еще балов осталось
-    //console.log("Набрано баллов за ход - " + points + ". Осталось набрать еще баллов - " + pointsEsc);
-    alert("Набрано баллов за ход - " + points + ". Осталось набрать еще баллов - " +  pointsEsc);
+		//Вывод информации в консоль для пользователя, о то сколько он заработал балов за ход и сколько ему еще балов осталось
+    	//console.log("Набрано баллов за ход - " + points + ". Осталось набрать еще баллов - " + pointsEsc);
+    	alert("Набрано баллов за ход - " + points + ". Осталось набрать еще баллов - " +  pointsEsc);
 
    
-    if (pointsNum >= scorePoints) {     //Набрано уже необходимое количество очков 50, прекращаем игру    
-       alert("Игра закончена! Набрано необходимое количество очков за шагов - " +step);
-   	   break;
-	}  
+   		if (pointsNum >= scorePoints) {     //Набрано уже необходимое количество очков 50, прекращаем игру    
+       		alert("Игра закончена! Набрано необходимое количество очков за шагов - " +step);
+   	    	break;
+		}  
 
 	}
 
@@ -56,29 +55,44 @@ pointsEsc = 0;
 function countPoints(num,numRandom,points) {
 
    var points = 0;
-   if (num === numRandom) {
-   	points = points +20;
-   	alert("Точно угадал число " + numRandom); 
-   } else {
-   	    if (num === numRandom+1 || num === numRandom-1) {
-   			points = points +15;
-   		}
-   		if (num === numRandom+2 || num === numRandom-2) {
-   			points = points +15;
-   		}
-   		if (num === numRandom+3 || num === numRandom-3 || num === numRandom+4 || num === numRandom-4) {
-   			points = points +3;
-   		}
-   		if (num === numRandom+5 || num === numRandom-5 || num === numRandom+6 || num === numRandom-6 ||
-   			num === numRandom+7 || num === numRandom-7 || num === numRandom+8 || num === numRandom-8 ||
-   			num === numRandom+9 || num === numRandom-9 || num === numRandom+10 || num === numRandom-10) {
-   			points = points +1;
-   		}
-   		else {
-   			points = points +0;
-   		}
-   }
-   return points;
+
+    switch (num) {
+  		case numRandom:
+  			points = points +20;
+    		alert("Точно угадал число " + numRandom); 
+    		break;
+  		case numRandom+1:
+  		case numRandom-1:
+    		points = points +15;
+   			break;
+  		case numRandom+2:
+  		case numRandom-2:
+    		points = points +10;
+    		break;
+    	case numRandom+3:
+  		case numRandom-3:
+  		case numRandom+4:
+  		case numRandom-4:
+    		points = points +3;
+    		break;
+    	case numRandom+5:
+  		case numRandom-5:
+  		case numRandom+6:
+  		case numRandom-6:
+  		case numRandom+7:
+  		case numRandom-7:
+  		case numRandom+8:
+  		case numRandom-8:
+  		case numRandom+9:
+  		case numRandom-9:
+  		case numRandom+10:
+  		case numRandom-10:
+    		points = points +1;
+    		break;
+  		default:
+    		points = points +0;
+	}
+	return points;
 }
 
 
