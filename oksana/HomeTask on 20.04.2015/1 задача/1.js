00e2b290cd177ef05fbe086421351d7fc0ec2bfa
krasotka <chetvertakova.oksana@gmail.com>
2015-05-08 21:21:02 +0300

    fix

--- oksana/HomeTask on 20.04.2015/1 задача/1.js
+++ oksana/HomeTask on 20.04.2015/1 задача/1.js
@@ -3,24 +3,34 @@ var node = document.querySelector('ul');
 function isTextExists (node, text) {// true/false
 	if (node.nodeType === 1) {
 		for (var i = 0; i < node.children.length; i++) {
+			var child = node.children[i];
 			if (node.children[i].innerHTML.indexOf(text) !== -1) {
 				return true;
 			}
+			isTextExists (child, text)
 		}
 	}
 	return false;
 }
 
 function getTextNode (node, text) { // node object
-	if (node.nodeType === 1) {
-		for (var i = 0; i < node.children.length; i++) {
-			if (node.children[i].innerHTML.indexOf(text) !== -1) {
-				return node.children[i];
+	var nodeObj = [];
+
+	function getText (node) {
+		if (node.nodeType === 1) {
+
+			for (var i = 0; i < node.children.length; i++) {
+
+				var child = node.children[i];
+
+				if (node.children[i].innerHTML.indexOf(text) !== -1) {
+
+					nodeObj.push(node.children[i]); 
+				}
+				getText(child);
 			}
 		}
 	}
+	getText(node);	
+	return nodeObj;
 }
-
-
-
-
